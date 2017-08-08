88f9bfdf7c506b93cbe3abc18a55e51c6cc4b6c5
Huang jianguo <1060684755@qq.com>
2017-08-08 23:57:43 +0800

    第二次提交

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 import "./sass/dialog.scss";
 import Dialog from "./js/dialog.es6.js";
+
 if(module.hot) {
 	module.hot.accept();
 }
-new Dialog({
-	imgSrc: "./static/images/right.png",
-	btnArr: [
-		{	
-			text: "取消",
-			callback: function() {
-				// console.log(1);
-				alert(1);
+
+document.querySelector("#test").addEventListener("click", () => {
+	new Dialog({
+		maskShow: false,
+		imgSrc: "./static/images/right.png",
+		btnArr: [
+			{	
+				text: "取消",
+				callback: function() {
+					console.log(1);
+				}
+			},
+			{
+				text: "确定",
+				callback: function() {
+					console.log(2);
+				}
+			},
+			{
+				text: "提交",
+				callback: function() {
+					console.log(3);
+				}
 			}
-		},
-		{
-			text: "确定",
-			callback: function() {
-				// console.log(2);
-				alert(2);
-			}
-		},
-		{
-			text: "提交",
-			callback: function() {
-				// console.log(3);
-				alert(3);
-			}
-		}
-	]
+		]
+	});
 });
