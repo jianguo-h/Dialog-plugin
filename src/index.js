2eae5d78387e1056ca5b1331914b167be702f32e
Huang jianguo <1060684755@qq.com>
2017-08-10 23:42:18 +0800

    代码功能基本完成

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./sass/dialog.scss";
-import Dialog from "./js/dialog.es6.js";
+import Dialog from "./js/dialog.js";
 
 if(module.hot) {
 	module.hot.accept();
@@ -7,25 +7,16 @@ if(module.hot) {
 
 document.querySelector("#test").addEventListener("click", () => {
 	new Dialog({
-		maskShow: false,
 		imgSrc: "./static/images/right.png",
 		btnArr: [
 			{	
-				text: "取消",
 				callback: function() {
-					console.log(1);
+					alert(">>> cancel");
 				}
 			},
 			{
-				text: "确定",
 				callback: function() {
-					console.log(2);
-				}
-			},
-			{
-				text: "提交",
-				callback: function() {
-					console.log(3);
+					alert(">>> confirm");
 				}
 			}
 		]
