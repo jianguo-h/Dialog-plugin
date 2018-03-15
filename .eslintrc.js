f5e420f24bdaf7e24a3ce656caabaf36483c1046
Huang jianguo <1060684755@qq.com>
2018-03-15 23:41:01 +0800

    升级所需的 npm 包

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
-// http://eslint.org/docs/user-guide/configuring
 module.exports = {
 	root: true,
-	// parser: "es-lint",
+	parser: 'babel-eslint',
 	parserOptions: {
 		sourceType: 'module'
 	},
@@ -9,42 +8,24 @@ module.exports = {
 		browser: true,
 	},
 	extends: 'standard',
-	plugins: ['html'],
+	plugins: [
+		'html'
+	],
 	'rules': {
-		'no-new': 0,
-		'one-var': 0,
-		'no-multi-spaces': 0,
-		'no-mixed-operators': 0,
-		'space-in-parens': 0,
-		'no-unneeded-ternary': 0,
-		'no-mixed-spaces-and-tabs': 0,
-		'no-useless-return': 0,
-		'no-dupe-class-member': 0,
-		'keyword-spacing': 0,
-		'spaced-comment': 0,
-		'brace-style': 0,
-		'no-unused-vars': 0,
-		'no-trailing-spaces': 0,
-		'no-extend-native': 0,
-		'padded-blocks': 0,
-		// 是否强制分号结尾
 		'semi': 0,
-		// 引号类型
 		'quotes': 0,
-		// 缩进 
-		'indent': 0,
-		//函数定义时括号前面要不要有空格
-		'space-before-function-paren': [0, 'always'],
-		//文件以单一的换行符结束
+		'no-new': 0,
+		'no-var': 2,
 		'eol-last': 0,
-		// Some style guides don’t allow the use of tab characters at all, including within comments.
-		'no-tabs': 0,
-		// allow paren-less arrow functions
-		'arrow-parens': 0,
-		// allow async-await
-		'generator-star-spacing': 0,
-		// allow debugger during development
-		'no-debugger':  0
-		// 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+		'brace-style': 0,
+		'prefer-const': 2,
+		'comma-dangle': 2,      // 禁止结尾有多余的逗号
+		'no-unused-vars': 1,
+		'spaced-comment': 0,    // 要求或禁止在注释前有空白
+		'no-multi-spaces': 0,
+		'keyword-spacing': 0,
+		'indent': ['error', 2],
+		'no-useless-return': 0,
+		'space-before-function-paren': 0,
 	}
 }
