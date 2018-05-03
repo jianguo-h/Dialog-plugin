8738a76ba56ac931de960f1bd3aa76dc71512821
jianguo <1060684755@qq.com>
2018-05-03 23:42:47 +0800

    增加一些工具方法

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,55 @@ export const type = val => {
   const end = string.length - 1;
   return string.slice(start, end).toLowerCase();
 }
+
+export const TEXT_NODE = 'text_node';
+export const EMPTY_NODE = 'empty_node';
+export const ELEMENT_NODE = 'element_node';
+
+export const judgeNodeType = vnode => {
+  const { tag, children } = vnode;
+
+  if(!tag) {
+    return TEXT_NODE;
+  }
+  else if(tag && !children) {
+    return EMPTY_NODE;
+  }
+  else {
+    return ELEMENT_NODE;
+  }
+}
+
+export const createTextNode = val => {
+  return document.createTextNode(val);
+}
+
+export const createEmptyNode = (tag, props) => {
+  const node = document.createElement(tag);
+  if(!props) {
+    return node;
+  }
+  return setProps(node, props);
+}
+
+const setProps = (node, props) => {
+  for(const [key, val] of Object.entries(props)) {
+    if(type(val) === 'object') {
+      setProps(node, val);
+    }
+    else {
+      if(key === 'className') {
+        node.setAttribute('class', val);
+      }
+      else if(key === 'on') {
+        for(const [event, cb] of Object.entries(val)) {
+          node['on' + event] = cb;
+        }
+      }
+      else {
+        node.setAttribute(key, val);
+      }
+    }
+  }
+  return node;
+}
