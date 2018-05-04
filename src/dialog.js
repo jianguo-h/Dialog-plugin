a762d6a7d21731bbc987302a3d4e6301183ea4d3
jianguo <1060684755@qq.com>
2018-05-04 20:26:37 +0800

    完成confirm方法

--- src/dialog.js
+++ src/dialog.js
@@ -15,26 +15,106 @@
 </div>
 */
 import {
-  type, TEXT_NODE, EMPTY_NODE, ELEMENT_NODE,
+  type, TEXT_NODE, EMPTY_NODE, /*  ELEMENT_NODE, */
   judgeNodeType, createTextNode, createEmptyNode
 } from './utils';
 import './dialog.scss';
 
 class Dialog {
-  constructor(opts) {
-    this.defaults = {
-      duration: 3000,         // 显示的时间, default: 3000
-      mask: true,             // 是否需要遮罩层, default: true
-      maskClose: true         // 点击遮罩层是否关闭(maskShow为true时方有效), default: true
-    };
-    this.opts = {
-      ...this.defaults,
+  constructor() {
+    this.dialogClass = '';              // 挂载元素的class
+  }
+  // 确认模态框
+  confirm(opts) {
+    const params = {
+      maskClose: true,                     // 点击遮罩层是否关闭(maskShow为true时方有效), default: true
+      content: '这里放提示的内容',          // 提示的内容, default: '这里放提示的内容'
+      confirmText: '确定',                 // 确定按钮的文字, default: '确定'
+      onConfirm: null,                     // 点击确定的回调, default: null, 配置了该参数需手动关闭
+      cancelText: '取消',                  // 取消按钮的文字, default: '取消'
+      onCancel: null,                      // 点击取消的回调, default: null, 配置了该参数需手动关闭
       ...opts
     }
-    this.dialogClass = '';
-  }
-  confirm() {
+    let { confirmText, content, onConfirm, maskClose, cancelText, onCancel } = params;
+    maskClose = type(maskClose) === 'boolean' ? maskClose : true;
+    content = type(content) === 'string' ? content : '这里放提示的内容';
+    confirmText = type(confirmText) === 'string' ? confirmText : '确定';
+    onConfirm = type(onConfirm) === 'function' ? onConfirm : null;
+    cancelText = type(cancelText) === 'string' ? cancelText : '取消';
+    onCancel = type(onCancel) === 'function' ? onCancel : null;
 
+    this.dialogClass = 'dialog-confirm-wrap';
+    const vnode = {
+      tag: 'div',
+      props: {
+        className: this.dialogClass
+      },
+      children: [
+        {
+          tag: 'div',
+          props: {
+            className: 'dialog-mask',
+            on: {
+              click: () => {
+                if(maskClose) {
+                  this.close();
+                }
+              }
+            }
+          },
+          children: null
+        },
+        { tag: 'div', props: { className: 'dialog-box gradientShow' },
+          children: [
+            { tag: 'div', props: { className: 'dialog-content' },
+              children: [
+                { tag: 'p', props: { className: 'dialog-message' }, children: content }
+              ]
+            },
+            { tag: 'div', props: { className: 'dialog-footer' },
+              children: [
+                {
+                  tag: 'span',
+                  props: {
+                    className: 'dialog-cancel-btn',
+                    on: {
+                      click: () => {
+                        if(onCancel) {
+                          onCancel();
+                        }
+                        else {
+                          this.close();
+                        }
+                      }
+                    }
+                  },
+                  children: cancelText
+                },
+                {
+                  tag: 'span',
+                  props: {
+                    className: 'dialog-confirm-btn',
+                    on: {
+                      click: () => {
+                        if(onConfirm) {
+                          onConfirm();
+                        }
+                        else {
+                          this.close();
+                        }
+                      }
+                    }
+                  },
+                  children: confirmText
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+    const el = this.createElement(vnode);
+    this.mounted(el);
   }
   // 提示
   message(opts) {
@@ -141,7 +221,8 @@ class Dialog {
                       }
                     }
                   },
-                  children: confirmText }
+                  children: confirmText
+                }
               ]
             }
           ]
