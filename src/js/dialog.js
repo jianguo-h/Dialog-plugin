d0cdd80ae0225935bc35fe37f1a6e7fff8133983
jianguo <1060684755@qq.com>
2018-05-05 21:17:35 +0800

    抽取confirm和alert的公共代码

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -17,7 +17,7 @@
 import {
   type, TEXT_NODE, EMPTY_NODE, /*  ELEMENT_NODE, */
   judgeNodeType, createTextNode, createEmptyNode,
-  setProps, judgePlatform
+  /*setProps, */judgePlatform, setNodeCenter
 } from './utils';
 import '../less/dialog.less';
 const platform = judgePlatform();
@@ -27,129 +27,6 @@ console.log('>>> platform', platform, isPc);
 class Dialog {
   constructor() {
     this.dialogClass = '';              // 挂载元素的class
-  }
-  // 确认模态框
-  confirm(opts) {
-    const params = {
-      maskClose: true,                     // 点击遮罩层是否关闭(maskShow为true时方有效), default: true
-      content: '这里放提示的内容',          // 提示的内容, default: '这里放提示的内容'
-      confirmText: '确定',                 // 确定按钮的文字, default: '确定'
-      onConfirm: null,                     // 点击确定的回调, default: null, 配置了该参数需手动关闭
-      cancelText: '取消',                  // 取消按钮的文字, default: '取消'
-      onCancel: null,                      // 点击取消的回调, default: null, 配置了该参数需手动关闭
-      ...(isPc ? {
-        title: '这里是标题',
-        showIconClose: true
-      } : {}),
-      ...opts
-    }
-    let { confirmText, content, onConfirm, maskClose, cancelText, onCancel, title, showIconClose } = params;
-    maskClose = type(maskClose) === 'boolean' ? maskClose : true;
-    content = type(content) === 'string' ? content : '这里放提示的内容';
-    confirmText = type(confirmText) === 'string' ? confirmText : '确定';
-    onConfirm = type(onConfirm) === 'function' ? onConfirm : null;
-    cancelText = type(cancelText) === 'string' ? cancelText : '取消';
-    onCancel = type(onCancel) === 'function' ? onCancel : null;
-    if(isPc) {
-      title = type(title) === 'string' ? title : '这里是标题';
-      showIconClose = type(showIconClose) === 'boolean' ? showIconClose : true;
-    }
-
-    this.dialogClass = 'dialog-' + platform + '-confirm';
-    const vnode = {
-      tag: 'div',
-      props: {
-        className: this.dialogClass
-      },
-      children: [
-        {
-          tag: 'div',
-          props: {
-            className: 'dialog-mask',
-            on: {
-              click: () => {
-                if(maskClose) {
-                  this.close();
-                }
-              }
-            }
-          },
-          children: null
-        },
-        { tag: 'div', props: { className: 'dialog-box gradientShow' },
-          children: [
-            isPc ? {
-              tag: 'div',
-              props: { className: 'dialog-header' },
-              children: [
-                {
-                  tag: 'p',
-                  props: { className: 'dialog-title' },
-                  children: title
-                },
-                showIconClose ? {
-                  tag: 'span',
-                  props: {
-                    className: 'dialog-header-close',
-                    on: {
-                      click: () => {
-                        this.close();
-                      }
-                    }
-                  },
-                  children: null
-                } : null
-              ]
-            } : null,
-            { tag: 'div', props: { className: 'dialog-content' },
-              children: [
-                { tag: 'p', props: { className: 'dialog-message' }, children: content }
-              ]
-            },
-            { tag: 'div', props: { className: 'dialog-footer' },
-              children: [
-                {
-                  tag: 'span',
-                  props: {
-                    className: 'dialog-cancel-btn',
-                    on: {
-                      click: () => {
-                        if(onCancel) {
-                          onCancel();
-                        }
-                        else {
-                          this.close();
-                        }
-                      }
-                    }
-                  },
-                  children: cancelText
-                },
-                {
-                  tag: 'span',
-                  props: {
-                    className: 'dialog-confirm-btn',
-                    on: {
-                      click: () => {
-                        if(onConfirm) {
-                          onConfirm();
-                        }
-                        else {
-                          this.close();
-                        }
-                      }
-                    }
-                  },
-                  children: confirmText
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    };
-    const el = this.createElement(vnode);
-    this.mounted(el);
   }
   // 提示
   message(opts) {
@@ -185,16 +62,8 @@ class Dialog {
     };
     const el = this.createElement(vnode);
     this.mounted(el);
-    // 设置居中
-    const { offsetWidth, offsetHeight } = el;
-    const props = {
-      style: {
-        marginLeft: -1 * Math.round(offsetWidth / 2) + 'px',
-        marginTop: -1 * Math.round(offsetHeight / 2) + 'px'
-      }
-    }
-    setProps(el, props);
 
+    setNodeCenter(el);
     setTimeout(() => {
       if(!callback) {
         this.close();
@@ -204,20 +73,33 @@ class Dialog {
       }
     }, duration);
   }
+  // 确认模态框
+  confirm(opts) {
+    this.modal(opts);
+  }
   // 信息框
   alert(opts) {
+    this.modal(opts, 'alert');
+  }
+  // 模态框
+  modal(opts, modalType = 'confirm') {
+    const isConfirm = modalType === 'confirm';
     const params = {
-      maskClose: true,                     // 点击遮罩层是否关闭(maskShow为true时方有效), default: true
+      maskClose: true,                      // 点击遮罩层是否关闭(maskShow为true时方有效), default: true
       content: '这里放提示的内容',          // 提示的内容, default: '这里放提示的内容'
-      confirmText: '确定',                 // 确定按钮的文字, default: '确定'
-      onConfirm: null,                     // 点击确定的回调, default: null, 配置了该参数需手动关闭
+      confirmText: '确定',                  // 确定按钮的文字, default: '确定'
+      onConfirm: null,                      // 点击确定的回调, default: null, 配置了该参数需手动关闭
       ...(isPc ? {
-        title: '这里是标题',
-        showIconClose: true
+        title: '这里是标题',                // 标题, default：'这里是标题'
+        showIconClose: true                 // 是否显示右上角关闭按钮, default：true
       } : {}),
-      ...opts
+      ...(isConfirm ? {
+        cancelText: '取消',                 // 取消按钮的文字, default: '取消'
+        onCancel: null                      // 点击取消的回调, default: null, 配置了该参数需手动关闭
+      } : {}),
+      ...(type(opts) === 'object' ? opts : {})
     }
-    let { confirmText, content, onConfirm, maskClose, title, showIconClose } = params;
+    let { confirmText, content, onConfirm, maskClose, cancelText, onCancel, title, showIconClose } = params;
     maskClose = type(maskClose) === 'boolean' ? maskClose : true;
     content = type(content) === 'string' ? content : '这里放提示的内容';
     confirmText = type(confirmText) === 'string' ? confirmText : '确定';
@@ -226,8 +108,12 @@ class Dialog {
       title = type(title) === 'string' ? title : '这里是标题';
       showIconClose = type(showIconClose) === 'boolean' ? showIconClose : true;
     }
+    if(isConfirm) {
+      cancelText = type(cancelText) === 'string' ? cancelText : '取消';
+      onCancel = type(onCancel) === 'function' ? onCancel : null;
+    }
 
-    this.dialogClass = 'dialog-' + platform + '-alert';
+    this.dialogClass = 'dialog-' + platform + '-' + modalType;
     const vnode = {
       tag: 'div',
       props: {
@@ -280,6 +166,23 @@ class Dialog {
             },
             { tag: 'div', props: { className: 'dialog-footer' },
               children: [
+                isConfirm ? {
+                  tag: 'span',
+                  props: {
+                    className: 'dialog-cancel-btn',
+                    on: {
+                      click: () => {
+                        if(onCancel) {
+                          onCancel();
+                        }
+                        else {
+                          this.close();
+                        }
+                      }
+                    }
+                  },
+                  children: cancelText
+                } : null,
                 {
                   tag: 'span',
                   props: {
