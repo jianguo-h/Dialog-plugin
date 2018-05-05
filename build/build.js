d811bc845c6172aea48f5467fdb8e460156a5513
jianguo <1060684755@qq.com>
2018-05-06 00:01:59 +0800

    安装打包所需的依赖

--- build/build.js
+++ build/build.js
@@ -1,8 +1,20 @@
 const webpack = require('webpack');
 const webpackProdConfig = require('./webpack.prod.config');
 
+console.log('building for production...\n');
 webpack(webpackProdConfig, (err, stats) => {
-  if(err) {
-    throw err;
-  }
+  if(err) throw err;
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n');
+
+  console.log('Build complete \n');
+  console.log(
+    'Tip: built files are meant to be served over an HTTP server.\n' +
+    'Opening index.html over file:// won\'t work.\n'
+  );
 });
