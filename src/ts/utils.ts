eb2a1499e013418734403f5d2ede2a1eb20bf6ad
jianguo <1060684755@qq.com>
2019-09-18 15:30:36 +0800

    refactor： 更新utils

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,91 +1,100 @@
+export const TEXT_NODE = 'text_node';
+export const EMPTY_NODE = 'empty_node';
+export const ELEMENT_NODE = 'element_node';
+
 // 判断变量的类型
-export const type = val => {
+export function getType<T>(val: T): string {
   const string = Object.prototype.toString.call(val);
   const start = string.indexOf(' ') + 1;
   const end = string.length - 1;
   return string.slice(start, end).toLowerCase();
 }
 
-export const TEXT_NODE = 'text_node';
-export const EMPTY_NODE = 'empty_node';
-export const ELEMENT_NODE = 'element_node';
+interface VNode {
+  tag?: string;
+  props?: {
+    [property: string]: any;
+  };
+  children?: VNode | string | null;
+}
+
+type NodeType = typeof TEXT_NODE | typeof EMPTY_NODE | typeof ELEMENT_NODE;
 
 // 判断一个vnode的类型
-export const judgeNodeType = vnode => {
+export function judgeNodeType(vnode: VNode): NodeType {
   const { tag, children } = vnode;
 
-  if(!tag) {
+  if (!tag) {
     return TEXT_NODE;
-  }
-  else if(tag && !children) {
+  } else if (tag && !children) {
     return EMPTY_NODE;
-  }
-  else {
+  } else {
     return ELEMENT_NODE;
   }
 }
 
-// 创建一个文本节点
-export const createTextNode = val => {
-  return document.createTextNode(val);
-}
-
-// 创建一个空节点
-export const createEmptyNode = (tag, props) => {
-  const node = document.createElement(tag);
-  if(!props) {
-    return node;
-  }
-  return setProps(node, props);
-}
-
 // 设置节点的属性
-export const setProps = (node, props) => {
-  for(const [key, val] of Object.entries(props)) {
-    if(key === 'className') {
+export function setProps(node: any, props: VNode['props'] = {}): HTMLElement {
+  for (const [key, val] of Object.entries(props)) {
+    if (key === 'className') {
       node.setAttribute('class', val);
-    }
-    else if(key === 'on') {
-      for(const [event, cb] of Object.entries(val)) {
-        if(type(cb) === 'function') {
+    } else if (key === 'on') {
+      for (const [event, cb] of Object.entries(val)) {
+        if (typeof cb === 'function') {
           node['on' + event] = cb;
         }
       }
-    }
-    else if(key === 'style') {
-      for(const [property, value] of Object.entries(val)) {
+    } else if (key === 'style') {
+      for (const [property, value] of Object.entries(val)) {
         node.style[property] = value;
       }
-    }
-    else {
+    } else {
       node.setAttribute(key, val);
     }
   }
   return node;
 }
 
+// 创建一个文本节点
+export function createTextNode(val: string): Text {
+  return document.createTextNode(val);
+}
+
+// 创建一个空节点
+export function createEmptyNode(
+  tag: VNode['tag'] = 'div',
+  props: VNode['props']
+): HTMLElement {
+  const node = document.createElement(tag);
+  if (!props) {
+    return node;
+  }
+  return setProps(node, props);
+}
+
 // 检测平台, pc or mobile
-export const judgePlatform = () => {
+export function judgePlatform(): 'mobile' | 'pc' {
   const userAgent = navigator.userAgent.toLowerCase();
   const agents = ['android', 'iphone', 'windows phone', 'ipad', 'ipod'];
-  for(const agent of agents) {
-    if(userAgent.includes(agent)) {
+  for (const agent of agents) {
+    if (userAgent.includes(agent)) {
       return 'mobile';
     }
   }
   return 'pc';
 }
-export const platform = judgePlatform();
-export const isPc = platform !== 'mobile';
 
 // 设置元素横向垂直居中
-export const setNodeCenter = node => {
+export function setNodeCenter(node: HTMLElement): void {
   const { offsetWidth, offsetHeight } = node;
   const props = {
     style: {
       marginLeft: -1 * Math.round(offsetWidth / 2) + 'px',
       marginTop: -1 * Math.round(offsetHeight / 2) + 'px'
     }
-  }
+  };
   setProps(node, props);
 }
+
+export const platform = judgePlatform();
+export const isPc = platform !== 'mobile';
