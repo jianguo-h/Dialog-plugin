65e4d6011ff8bbfd6e3119d162878ae59b8cb630
jianguo <1060684755@qq.com>
2019-09-19 14:40:11 +0800

    chore: webpack热更新配置修改

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,23 +1,23 @@
-const path = require('path');
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
 const webpackDevConfig = require('./webpack.dev.config');
 const port = 8080;
 const url = 'http://localhost:' + port;
 
-Object.keys(webpackDevConfig.entry).forEach(entryName => {
-  webpackDevConfig.entry[entryName] = ['webpack-dev-server/client?' + url, 'webpack/hot/dev-server'].concat(webpackDevConfig.entry[entryName]);
-});
-
-const compiler = webpack(webpackDevConfig);
-const server = new webpackDevServer(compiler, {
+const options = {
   hot: true,
   disableHostCheck: true,		// 允许使用ip访问
   open: true,
   stats: {
     colors: true
   }
-});
+};
+
+webpackDevServer.addDevServerEntrypoints(webpackDevConfig, options);
+
+const compiler = webpack(webpackDevConfig);
+
+const server = new webpackDevServer(compiler, options);
 
 server.listen(port, () => {
   console.log('> Listening at ' + url);
