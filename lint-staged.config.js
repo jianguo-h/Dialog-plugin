738c965c764d1ac696a1b12fcaed141d2dcce7c2
jianguo-h <1060684755@qq.com>
2022-01-10 15:04:28 +0800

    feat: add husky, lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  './**/*.{js?(x),ts?(x)}': [
+    'prettier --ignore-path .eslintignore --write',
+    'eslint --fix',
+  ],
+  'src/**/*.{css,less,scss,sass}': ['prettier --write'],
+};
