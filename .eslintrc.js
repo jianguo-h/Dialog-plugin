b8a782a32dd7b3c6af33286a347b5847d55e2bf8
jianguo-h <1060684755@qq.com>
2022-01-10 15:56:28 +0800

    build: update pkgs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,65 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2018
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    node: true,
   },
-  'rules': {
+  rules: {
     '@typescript-eslint/no-var-requires': 'warn',
-    '@typescript-eslint/explicit-function-return-type': ['error', {
-      allowExpressions: true
-    }],
-    '@typescript-eslint/interface-name-prefix': 'off'
-  }
-}
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      {
+        ignoreParameters: true,
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['StrictPascalCase'],
+        prefix: ['I'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'typeLike',
+        format: ['StrictPascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'property',
+        format: ['strictCamelCase', 'snake_case', 'StrictPascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'variableLike',
+        format: [
+          'strictCamelCase',
+          'snake_case',
+          'StrictPascalCase',
+          'UPPER_CASE',
+        ],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
+  },
+};
