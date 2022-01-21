bc549b04ce93c4e8eb68a4e07485d1f2b24cd3c5
jianguo-h <1060684755@qq.com>
2022-01-21 08:39:12 +0800

    feat: add stylelint-less and postcss-less pkgs

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  customSyntax: 'postcss-less',
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-less'],
   rules: {
     'keyframes-name-pattern': null,
     'comment-empty-line-before': 'always',
